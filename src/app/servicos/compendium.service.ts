2c56cb172ac23651d24e6df29dade53eaf79b88d
SrDoppelganger <arthur.albuquerque2004@gmail.com>
2024-11-18 16:08:49 -0300

    updated db.json

--- src/app/servicos/compendium.service.ts
+++ src/app/servicos/compendium.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class CompendiumService {
 
   constructor(private http: HttpClient) { }
-  apiLink:string='http://localhost:3000?_embed=Alice';
+  apiLink:string='http://localhost:3000/personas?_page=1&_limit=5';
   getAllPersonas(){
     return this.http.get(this.apiLink);
   }
